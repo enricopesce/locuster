1bde5a45c3edfa8a29adeb1f6e1b7880a635a42e
Enrico Pesce <enrico.pesce@pixartprinting.com>
2020-10-27 16:34:11 +0100

    test selenium

---
+++ docker/code/test.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_prefs = {}
+chrome_options.experimental_options["prefs"] = chrome_prefs
+chrome_prefs["profile.default_content_settings"] = {"images": 2}
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("http://www.python.org")
+print(driver.page_source)
+assert "Python" in driver.title
+elem = driver.find_element_by_name("q")
+elem.clear()
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
+assert "No results found." not in driver.page_source
+driver.close()
