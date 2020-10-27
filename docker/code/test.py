53a43fc1c217306b97e7eb13dc4dde23d053637c
Enrico Pesce <enrico.pesce@pixartprinting.com>
2020-10-27 16:48:59 +0100

    remove the unnecessary

--- docker/code/test.py
+++ docker/code/test.py
@@ -6,9 +6,6 @@ chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_prefs = {}
-chrome_options.experimental_options["prefs"] = chrome_prefs
-chrome_prefs["profile.default_content_settings"] = {"images": 2}
 
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("http://www.python.org")
